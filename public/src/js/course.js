0406d0a41e9bb7421e8cb26d2ffd24ce89383239
ponyspy <desade4me@gmail.com>
2014-10-31 18:35:22 +0300

    panel

--- public/src/js/course.js
+++ public/src/js/course.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-	$('.block_item_exercises').each(function(index, el) {
-		$(this).children('.category_exercises_block:first').show();
-	});
-
-	$('.category_item').click(function(event) {
-		var current = $(this).index();
-		$(this).closest('.block_item').children('.block_item_exercises').children('.category_exercises_block').hide().eq(current).show();
-		// $('.category_exercises_block').hide().eq(current).show();
+	$('.circles').on({
+		mouseenter: function() {
+			$('.lesson_blocks_item').slideDown(300);
+		},
+		mouseleave: function() {
+			$('.lesson_blocks_item').slideUp(300);
+		}
 	});
 });
