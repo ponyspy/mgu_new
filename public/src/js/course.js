2241c0125cf5bdc3fde0a01c2a05deec41477b94
ponyspy <desade4me@gmail.com>
2014-11-03 17:07:09 +0300

    styl

--- public/src/js/course.js
+++ public/src/js/course.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 	$('.circle').on({
-		mouseenter: function() {
+		mouseenter: function(event) {
 			$('.lesson_blocks_item').stop().slideDown(300);
 			$('.dictionary_block').css('border-top', '1px solid black');
 		},
 		mouseleave: function(event) {
-			$('.lesson_blocks_item').stop().slideUp(300);
-			$('.dictionary_block').css('border-top', 'none');
+				$('.lesson_blocks_item').stop().slideUp(300);
+				$('.dictionary_block').css('border-top', 'none');
+		},
+		click: function() {
+			$(this).off();
 		}
 	});
 });
