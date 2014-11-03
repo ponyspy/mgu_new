3bda6d30b360985d03470611ff546e4bdc3b3f92
ponyspy <desade4me@gmail.com>
2014-11-03 14:10:40 +0300

    styles

--- public/src/js/course.js
+++ public/src/js/course.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-	$('.circles').on({
+	$('.circle').on({
 		mouseenter: function() {
-			$('.lesson_blocks_item').slideDown(300);
+			$('.lesson_blocks_item').stop().slideDown(300);
+			$('.dictionary_block').css('border-top', '1px solid black');
 		},
-		mouseleave: function() {
-			$('.lesson_blocks_item').slideUp(300);
+		mouseleave: function(event) {
+			$('.lesson_blocks_item').stop().slideUp(300);
+			$('.dictionary_block').css('border-top', 'none');
 		}
 	});
 });
