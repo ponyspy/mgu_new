009189e133d6dd4223756361262fbe1b6d683ed2
ponyspy <desade4me@gmail.com>
2015-02-04 21:03:10 +0300

    path

--- routes/teaser.js
+++ routes/teaser.js
@@ -12,6 +12,7 @@ var nodemailer = require('nodemailer');
 			secure: true
 });
 
+var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
 exports.index = function(req, res) {
